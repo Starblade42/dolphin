25e44dbef0835e519ae3a1c4e1f06694e1a37bfd
Jonathan Huff <zarcos@gmail.com>
2013-11-18 20:24:06 -0700

    I completed more class definitions. I think we should use the auto-created auto-numbering <classname>.id fields instead our our own made up ones to conform more with the django conventions, but we don’t have to!

--- classDefinitions.py
+++ classDefinitions.py
@@ -1,6 +1,7 @@
 from django.db import models
 
 #Authors	authorID	firstName	lastName
+#This will create and Auto-Incrementing ID primary key!
 
 class Author(models.Model):
 	authorID = models.CharField(max_length=50)
@@ -8,19 +9,41 @@ class Author(models.Model):
 	firstName = models.CharField(max_length=50)
 	
 
-
-
 #Blog	blogId	authorID	title	subtitle	editor
+#This will create and Auto-Incrementing ID primary key!
 
-#Updates	authorID	articleId	timestamp
+class Blog(models.Model):
+	blogId = models.CharField(max_length=50)
+	authorID = models.ForeignKey(Author)
+	title = models.CharField(max_length=120)
+	subtitle = models.CharField(max_length=120)
+	editor = models.ForeignKey(Author, blank=true)
+	
+#Article	articleId	title	subtitle	authorId	body	sources
 
-#Reviews	reviewId	title	subtitle	body	score	datePosted	productId
+class Updates(models.Model):
+	articleId = models.CharField(max_length=140)
+	title = models.Charfield(max_length=256)
+	subtitle = models.Charfield(max_length=256)
+	authorId = models.ForeignKey(Author)
+	body = model.TextField()
+	sources = models.TextField()
 
-#Articles	articleId	title	subtitle	authorId	body	sources
+#Updates	authorID	articleID	timestamp
+#This will create and Auto-Incrementing ID primary key!
+
+class Updates(models.Model):
+	authorID = models.ForeignKey(Author)
+	articleID = models.ForeignKey(Article)
+	timestamp = models.TimeField()
+
+
+
+#Review	reviewId	title	subtitle	body	score	datePosted	productId
 
 #Media	mediaId	title	subtitle	authorId	type	location
 
-#Tutorials	tutorialId	title	subtitle	authorId	body	productId
+#Tutorial	tutorialId	title	subtitle	authorId	body	productId
 
 #FavoriteLink	authorID	title	description	URL
 
