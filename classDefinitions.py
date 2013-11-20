e90cdaabe017871431e4792391620ee063fc7cc3
brady <brady@brady-HP-Pavilion-dv6-Notebook-PC.it.et.byu.edu>
2013-11-20 15:10:44 -0700

    added a few classes and changed database username

--- classDefinitions.py
+++ classDefinitions.py
@@ -21,7 +21,7 @@ class Blog(models.Model):
 	
 #Article	articleId	title	subtitle	authorId	body	sources
 
-class Updates(models.Model):
+class Article(models.Model):
 	articleId = models.CharField(max_length=140)
 	title = models.Charfield(max_length=256)
 	subtitle = models.Charfield(max_length=256)
@@ -41,10 +41,50 @@ class Updates(models.Model):
 
 #Review	reviewId	title	subtitle	body	score	datePosted	productId
 
+class Review(models.Model)
+	reviewId = models.CharField(max_length=256)
+	title = models.CharField(max_length=256)
+	subtitle = models.CharField(max_length=256)
+	body = models.TextField()
+	score = models.CharField(max_length=256)
+	datePosted = models.DateField()
+	productId = models.CharField(max_length=256)
+
+
 #Media	mediaId	title	subtitle	authorId	type	location
+
+class Media(models.Model)
+	mediaId = model.CharField(max_length=256)
+	title = models.CharField(max_length=256)
+	subtitle = models.CharField(max_length=256)
+	authorID = models.ForeignKey(Author)
+	mediaType = models.CharField(max_length=256)
+	location = models.CharField(max_length=256)
 
 #Tutorial	tutorialId	title	subtitle	authorId	body	productId
 
+class Tutorial(models.Model)
+	tutorialId = models.CharField(max_length=256)
+	title = models.
+	subtitle = models.CharField(max_length=256)
+	authorID = models.ForeignKey(Author)
+	body = models.TextField()
+	productId = models.CharField(max_length=256)
+
 #FavoriteLink	authorID	title	description	URL
 
+class FavoriteLink(models.Model)
+	authorID = models.ForeignKey(Author)
+	title = models.CharField(max_length=256)
+	description = models.TextField()
+	URL = models.CharField(max_length=256)
+
 #Bio	authorID	title	subtitle	body	hometown	hobbies
+
+class Bio(models.Model):
+	authorID = models.ForeignKey(Author)
+	title = models.CharField(max_length=256)
+	subtitle = models.CharField(max_length=256)
+	body = models.TextField()
+	hometown = models.CharField(max_length=50)
+	hobbies = models.TextField()
--- dolphin/settings.py
+++ dolphin/settings.py
@@ -13,7 +13,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.','mysql' # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
         'NAME': 'dolphin',                      # Or path to database file if using sqlite3.
-        'USER': 'it250',                      # Not used with sqlite3.
+        'USER': 'it350',                      # Not used with sqlite3.
         'PASSWORD': 'IT350Databases',                  # Not used with sqlite3.
         'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
         'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
