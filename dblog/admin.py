c08aa43fc5aa9b64a4529f163b653b342cc1f3cd
Starblade42 <zarcos@gmail.com>
2013-11-20 16:16:44 -0800

    updated thinsg fofe feshteafej'faw

--- dblog/admin.py
+++ dblog/admin.py
@@ -1,5 +1,11 @@
 from django.contrib import admin
-import dblog.models
+from dblog.models.dblog import Author
+from dblog.models.dblog import Blog
+from dblog.models.dblog import Article
+from dblog.models.dblog import Updates
+from dblog.models.dblog import Review
+from dblog.models.dblog import Media
+from dblog.models.dblog import Tutorial
 
 admin.site.register(Author)
 admin.site.register(Blog)
