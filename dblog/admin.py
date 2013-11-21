434529372f5ec1f9a5a770a340b34281ae048e0c
Starblade42 <zarcos@gmail.com>
2013-11-20 16:33:16 -0800

    updated admin.py with changes to make it work (updates are not includes right now as admin interface items).
    Also updated urls.py to make the admin interface work (uncommented
    several lines that should have been uncommented in the tutorial, but the
    tutorial never mentioned them.).

--- dblog/admin.py
+++ dblog/admin.py
@@ -1,16 +1,14 @@
 from django.contrib import admin
-from dblog.models.dblog import Author
-from dblog.models.dblog import Blog
-from dblog.models.dblog import Article
-from dblog.models.dblog import Updates
-from dblog.models.dblog import Review
-from dblog.models.dblog import Media
-from dblog.models.dblog import Tutorial
+from dblog.models import Author
+from dblog.models import Blog
+from dblog.models import Article
+from dblog.models import Review
+from dblog.models import Media
+from dblog.models import Tutorial
 
 admin.site.register(Author)
 admin.site.register(Blog)
 admin.site.register(Article)
-admin.site.register(Updates)
 admin.site.register(Review)
 admin.site.register(Media)
 admin.site.register(Tutorial)
