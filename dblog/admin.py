1ebeaa0000517d82bf39411d6a833dfd86467e6d
Starblade42 <zarcos@gmail.com>
2013-11-20 16:01:37 -0800

    I updated the admin.py in dblogs so we have admin junks for thems

---
+++ dblog/admin.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+import dblog.models
+
+admin.site.register(Author)
+admin.site.register(Blog)
+admin.site.register(Article)
+admin.site.register(Updates)
+admin.site.register(Review)
+admin.site.register(Media)
+admin.site.register(Tutorial)
