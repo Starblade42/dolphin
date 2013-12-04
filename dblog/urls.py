de6b17016b8a801a5fd8ea85cf78d8e7fd3d9f28
mavsman4457 <brady@brady-HP-Pavilion-dv6-Notebook-PC.it.et.byu.edu>
2013-12-04 16:33:18 -0700

    changed a tiny thing in views

--- dblog/urls.py
+++ dblog/urls.py
@@ -3,5 +3,6 @@ from django.conf.urls import patterns, url
 from dblog import views
 
 urlpatterns = patterns('',
-    url(r'^$', views.index, name='index')
+    url(r'^$', views.index, name='index'),
+    url(r'^(?P<author_id>\d+)/$', views.detail, name='detail')
 )
--- dblog/views.py
+++ dblog/views.py
@@ -1,5 +1,6 @@
 from django.http import Http404
 from django.shortcuts import render
+from django.shortcuts import get_object_or_404
 
 from dblog.models import Author
 
