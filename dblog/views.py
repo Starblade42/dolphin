f8c9a00f8839e531ce5484da06614e21a878e5fb
mavsman4457 <brady@brady-HP-Pavilion-dv6-Notebook-PC.app.byu.edu>
2013-12-04 15:36:16 -0700

    loads up the author list in alphabetical order by last name on main dblog page

--- dblog/views.py
+++ dblog/views.py
@@ -1,5 +1,12 @@
 from django.http import HttpResponse
+from django.template import RequestContext, loader
 
+from dblog.models import Author
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the dblog index.")
+    author_list = Author.objects.order_by('lastName')
+    template = loader.get_template('dblog/index.html')
+    context = RequestContext(request, {
+        'author_list': author_list,
+    })
+    return HttpResponse(template.render(context))
