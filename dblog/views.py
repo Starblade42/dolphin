8cfaaa237cfa273c3ac14717df3795f4af5a0908
mavsman4457 <brady@brady-HP-Pavilion-dv6-Notebook-PC.it.et.byu.edu>
2013-12-07 17:33:05 -0700

    changed syntax of links list

--- dblog/views.py
+++ dblog/views.py
@@ -20,9 +20,9 @@ def index(request):
 def detail(request, author_ID):
 	author = get_object_or_404(Author, authorID__exact=author_ID)
 	blog_list = Blog.objects.order_by('title')
-	links = FavoriteLink.objects.order_by('authorID')
+	link_list = FavoriteLink.objsects.order_by('authorID')
 	bio_list = Bio.objects.order_by('authorID')
-	return render(request, 'dblog/detail.html', {'author': author, 'blog_list': blog_list, 'links': links, 'bio_list': bio_list}) 
+	return render(request, 'dblog/detail.html', {'author': author, 'blog_list': blog_list, 'link_list': link_list, 'bio_list': bio_list}) 
 
 def blog(request, author_ID, blog_ID):
         author = get_object_or_404(Author, authorID__exact=author_ID)
